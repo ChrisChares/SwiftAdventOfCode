d836702c0f7e7bc4c113e686de42c0760ea40d40
Chris Chares <chris@chares.io>
2017-01-04 13:41:29 -0700

    Initial commit

---
+++ Shared.swift
@@ -0,0 +1,6 @@
+/*
+	Put any code you want to share between Advent days here.
+	This file is symlinked into each individual source folder.
+	Good candiates for code here include generic errors or 
+	reusable parsing logic.
+*/
