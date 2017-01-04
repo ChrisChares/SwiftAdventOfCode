8bd2181544a81eef8944b9cb312af3f77f92ba58
Chris Chares <chris@chares.io>
2017-01-04 13:44:30 -0700

    Xcodeproj wrapper for Shared.swift

--- Shared/Sources/Shared.swift
+++ Shared/Sources/Shared.swift
@@ -1,5 +1,5 @@
 /*
-	Put any code you want to share between Advent days here.
+	Put any code you want to share between Advent days in this file.
 	This file is symlinked into each individual source folder.
 	Good candiates for code here include generic errors or 
 	reusable parsing logic.
